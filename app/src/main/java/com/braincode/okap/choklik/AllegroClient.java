a9b4dcc98822a73432d2af4b7cc1078bb68fbb06
Hubert D <hubert.dworczynski@gmail.com>
2015-03-13 23:22:21 +0100

    json retrieval

--- app/src/main/java/com/braincode/okap/choklik/AllegroClient.java
+++ app/src/main/java/com/braincode/okap/choklik/AllegroClient.java
@@ -2,20 +2,34 @@ package com.braincode.okap.choklik;
 
 import android.util.Log;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import static us.monoid.web.Resty.*;
-import us.monoid.web.Resty.*;
+import retrofit.RestAdapter;
+import retrofit.http.POST;
+import retrofit.http.Query;
+import retrofit.http.QueryMap;
 
-import us.monoid.web.Resty;
-
-/**
- * Created by hubert on 13.03.15.
- */
 public class AllegroClient {
     public static final String TAG = "AlegroClient";
 
@@ -53,23 +67,56 @@ public class AllegroClient {
         return new String(getUrlBytes(urlSpec));
     }
 
+    interface SearchInterface {
+        @POST(METHOD_OFFERS)
+        void getSearchResults(@Query("searchString") String searchString);
+    }
+
     public static void fetchSearchResults() {
+        HttpClient client = new DefaultHttpClient();
+        HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000);
+        HttpResponse response;
+        JSONObject json = new JSONObject();
+
+        String tokenParam = "?access_token=" + sessionToken;
+        String url = ENDPOINT + METHOD_OFFERS + tokenParam;
+
         try {
-            Resty r = new Resty();
-            r.withHeader("Content-Type", "application/json");
+            HttpPost post = new HttpPost(url);
+            json.put("searchString", "nokia");
+            StringEntity se = new StringEntity(json.toString());
+            se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+            post.setEntity(se);
+            response = client.execute(post);
 
-            String tokenParam = "?access_token=" + sessionToken;
-
-            String url = ENDPOINT + METHOD_OFFERS + tokenParam;
-            Log.i(TAG, "Search method url: " + url);
-
-            Log.i(TAG, r.text(url, form(
-                    data("searchString", "nokia"))).toString()
-            );
-        } catch (IOException ioe) {
-            Log.e(TAG, "error while fetching search results", ioe);
+            if(response!=null) {
+                InputStream in = response.getEntity().getContent();
+                StringWriter writer = new StringWriter();
+                IOUtils.copy(in, writer);
+                String result = writer.toString();
+                Log.i(TAG, "result: " + result);
+            }
         } catch (Exception e) {
-            e.printStackTrace();
+            Log.e(TAG, "Error while sending POST request, ", e);
         }
     }
+//        try {
+//            RestAdapter restAdapter = new RestAdapter.Builder()
+//                    .setEndpoint(ENDPOINT)
+//                    .build();
+//
+//            SearchInterface searchService = restAdapter.create(SearchInterface.class);
+//            searchService.getSearchResults("nokia");
+//
+////            r.withHeader("Content-Type", "application/json");
+//
+//
+//            Log.i(TAG, "Search method url: " + url);
+//
+//        } catch (IOException ioe) {
+//            Log.e(TAG, "error while fetching search results", ioe);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
