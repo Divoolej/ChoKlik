252523bea26d57f3e9386fe2ad7b809e50cb8af3
Divoolej <divoolej@gmail.com>
2015-03-13 23:04:21 +0100

    Commit after 23:00

--- app/src/main/java/com/braincode/okap/choklik/Words.java
+++ app/src/main/java/com/braincode/okap/choklik/Words.java
@@ -1,12 +1,74 @@
 //package com.braincode.okap.choklik;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by divoolej on 13.03.15.
  */
 
 public class Words {
+    private static HashMap<Character, ArrayList<Character> > keyMap = InitializeKeyMap();
+
+    private static HashMap<Character, ArrayList<Character> > InitializeKeyMap() {
+        HashMap<Character, ArrayList<Character> > map = new HashMap<>();
+        ArrayList<Character> array = new ArrayList<>(1);
+
+        array.add('w');
+        map.put('q', array);
+
+        array.clear();
+        array.add('s');
+        map.put('a', array);
+
+        array.clear();
+        array.add('x');
+        map.put('z', array);
+
+        array.clear();
+        array.add('o');
+        map.put('p', array);
+
+        array.clear();
+        array.add('k');
+        map.put('l', array);
+
+        array.clear();
+        array.add('n');
+        map.put('m', array);
+
+        String s = "qwertyuiop";
+
+        for (int i = 1; i < s.length() - 1; i++) {
+            array.clear();
+            array.add(s.charAt(i - 1));
+            array.add(s.charAt(i + 1));
+            map.put(s.charAt(i), array);
+        }
+
+        s = "asdfghjkl";
+
+        for (int i = 1; i < s.length() - 1; i++) {
+            array.clear();
+            array.add(s.charAt(i-1));
+            array.add(s.charAt(i+1));
+            map.put(s.charAt(i), array);
+        }
+
+        s = "zxcvbnm";
+
+        for (int i = 1; i < s.length() - 1; i++) {
+            array.clear();
+            array.add(s.charAt(i-1));
+            array.add(s.charAt(i+1));
+            map.put(s.charAt(i), array);
+        }
+
+        return map;
+    }
+
     public static void main(String[] args) {
         try {
             getPossibleMisspelledWords("samsung daj");
@@ -53,6 +115,7 @@ public class Words {
                 subresult.add(String.valueOf(word));
             }
         }
+
         return subresult;
     }
 
